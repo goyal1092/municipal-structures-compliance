4842a69ff42ebfc28a7d2cce4c94414362bca931
Paul Watson <github.com@paulmwatson.com>
2021-09-14 13:28:05 +0200

    Open close navigation drawer

--- assets/js/msc.js
+++ assets/js/msc.js
@@ -1,3 +1,8 @@
-(function() {
-  console.log("hello world");
-})();
+// Instantiate MDC Drawer
+if (document.getElementById("mdc-drawer-opener")) {
+  const drawerEl = document.querySelector(".mdc-drawer");
+  const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
+  document.getElementById("mdc-drawer-opener").addEventListener("click", () => {
+    drawer.open = !drawer.open;
+  });
+}
