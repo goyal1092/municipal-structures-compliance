ebdd212ccbbd0ec8ad2784c3f1b26e2e867db7ce
Gaurav Goyal <goyal1092@gmail.com>
2021-09-20 15:54:44 +0530

    Fixed migration and user model

--- msc/authentication/models.py
+++ msc/authentication/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 from django.conf import settings
 
@@ -8,7 +8,40 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from msc.questionnaire.models import MSCBase
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
+
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -23,6 +56,11 @@ class User(AbstractUser):
         "organisation.Organisation", on_delete=models.CASCADE,
         null=True, blank=True
     )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.display_name()
