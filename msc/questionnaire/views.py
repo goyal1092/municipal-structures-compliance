2db347910f032d80ae34dec8844a14179d477433
Paul Watson <github.com@paulmwatson.com>
2021-09-20 11:29:51 +0200

    Temporarily disabled @check_user_org check

--- msc/questionnaire/views.py
+++ msc/questionnaire/views.py
@@ -17,7 +17,7 @@ from .models import Questionnaire
 from .utils import get_serialized_questioner, check_user_org
 
 
-@check_user_org
+#@check_user_org
 @login_required
 def questionnaire_list(request):
 
